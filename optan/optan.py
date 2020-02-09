fa002b3e652ec1a7a6155e1546e3031631713a9e
Shunta Komatsu <shuntak217@gmail.com>
2020-02-09 17:23:04 +0900

    :sparkles: Add class for managing additional parameters

--- optan/optan.py
+++ optan/optan.py
@@ -1,5 +1,42 @@
 import argparse
-from typing import Optional, Sequence
+import logging
+from typing import Any, Dict, Optional, Sequence
+
+
+class _Params:
+    def __init__(self):
+        """Manage parameters."""
+        self._params = {}
+
+    @property
+    def params(self):
+        return self._params
+
+    def add_param(self, key: str, value: Any, overwrite: bool = False):
+        """Add parameter.
+
+        Args:
+            key (str): Parameter key
+            value (Any): Parameter value
+            overwrite (bool, optional): Overwrite if key already exists. Defaults to False.
+        """
+        if key in self._params and not overwrite:
+            if not overwrite:
+                logging.warning(
+                    "Key {} already exists. If you want to overwrite, set argument 'overwrite' to 'True'.".format(key)
+                )
+        else:
+            self._params[key] = value
+
+    def add_params(self, params: Dict[str, Any], overwrite: bool = False):
+        """Add parameters.
+
+        Args:
+            params (Dict[str, Any]): Parameters dict
+            overwrite (bool, optional): Overwrite if key already exists. Defaults to False.
+        """
+        for k, v in params.items():
+            self.add_param(k, v, overwrite)
 
 
 class Optan(argparse.ArgumentParser):
